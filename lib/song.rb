867cd00179e0fc04e06b690948aa5b5473ac5e5f
Joshua Crum <joshua.crum@gmail.com>
2019-05-29 00:01:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     song = self.new(song)
-    artist = self.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(song)
   end
 
